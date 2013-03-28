6cd8edd51415bec0d54311668d3f52026880ceac
Christopher Tugangui <ctugangui@gmail.com>
2013-03-28 00:28:45 -0700

    Added Koush Superuser. Updated system files

--- products/common.mk
+++ products/common.mk
@@ -1,3 +1,14 @@
+# Generic product
+PRODUCT_NAME := perfectbutter
+PRODUCT_BRAND := perfectbutter
+PRODUCT_DEVICE := generic
+
+PRODUCT_PACKAGES += \
+    Superuser  \
+    su
+
+SUPERUSER_PACKAGE := com.perfectbutter.superuser
+
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal=http://www.google.com/intl/%s/mobile/android/basic/phone-legal.html \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
@@ -6,14 +17,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.error.receiver.system.apps=com.google.android.feedback \
     ro.com.google.locationfeatures=1 \
     ro.setupwizard.enterprise_mode=1 \
-    windowsmgr.max_events_per_sec=240 \
-    ro.kernel.android.checkjni=0
+    windowsmgr.max_events_per_sec=240 
 
-# Blobs common to all devices
+# init.d support
 PRODUCT_COPY_FILES += \
-#    vendor/perfectbutter/prebuilt/common/app/Superuser.apk:system/app/Superuser.apk \
-#    vendor/perfectbutter/prebuilt/common/xbin/su:system/xbin/su \
+    vendor/perfectbutter/prebuilt/common/etc/init.d/03firstboot:system/etc/init.d/03firstboot \
+    vendor/perfectbutter/prebuilt/common/etc/init_trigger.enabled:system/etc/init_trigger.enabled \
     vendor/perfectbutter/prebuilt/common/bin/sysinit:system/bin/sysinit
+
+# Added xbin files
+PRODUCT_COPY_FILES += \
+    vendor/perfectbutter/prebuilt/common/xbin/sysro:system/xbin/sysro \
+    vendor/perfectbutter/prebuilt/common/xbin/sysrw:system/xbin/sysrw
 
 # common boot animation
 PRODUCT_COPY_FILES += \
